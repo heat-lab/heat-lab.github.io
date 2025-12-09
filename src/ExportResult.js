978e72f05f6047938f7609bf2c70fddc70363caf
bennettwhong <bennettwhong@gmail.com>
2025-12-09 11:28:14 -0800

    Update ExportResult.js

--- src/ExportResult.js
+++ src/ExportResult.js
@@ -10,6 +10,7 @@ import TranslationButton from "./Components/TranslationButton";
 import BlueButton from "./Components/BlueButton";
 import "./ExportResult.scss";
 import "./Home/Home.css";
+import { APIBASEURL } from "./config";
 
 const ExportResult = () => {
   const [username, setUsername] = useState(null);
@@ -21,17 +22,23 @@ const ExportResult = () => {
       "Failed to download, make sure fields are not empty and user name is correct."
     );
   };
+
   const downloadReport = async () => {
     try {
-      let response = await fetch(
-        `https://ue2r8y56oe.execute-api.us-east-2.amazonaws.com/default/getReport?participant_id=${username}&language=${language}`
+      const response = await fetch(
+        `${APIBASEURL}/export?participantid=${encodeURIComponent(
+          username
+        )}&language=${encodeURIComponent(language)}`
       );
       if (response.ok) {
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", `${username}-${language}-report.xlsx`);
+        link.setAttribute(
+          "download",
+          `${username}-${language}-report.xlsx`
+        );
         document.body.appendChild(link);
         link.click();
         link.parentNode.removeChild(link);
@@ -42,13 +49,18 @@ const ExportResult = () => {
       alertExportFailure();
     }
   };
+
   return (
     <div className="exportResult">
-     <AppBar className="titleContainer">
+      <AppBar className="titleContainer">
         <h1 className="title">
-          {showChinese ? <>输入用户名并选择一门语言后，点击Download下载报告</> : <>Enter Username, select language, then click Download to download report</>}
+          {showChinese ? (
+            <>输入用户名并选择一门语言后，点击Download下载报告</>
+          ) : (
+            <>Enter Username, select language, then click Download to download report</>
+          )}
         </h1>
-        <div style={{height: "20px"}}></div>
+        <div style={{ height: "20px" }}></div>
         <TranslationButton
           showChinese={showChinese}
           setShowChinese={setShowChinese}
@@ -69,7 +81,11 @@ const ExportResult = () => {
                 className="exportLanguageSelection"
                 onChange={(e) => setLanguage(e.target.value)}
               >
-                <span>{showChinese ? "请选择想要下载报告的语言： " : "Please select the language you want to download the report in: "}</span>
+                <span>
+                  {showChinese
+                    ? "请选择想要下载报告的语言： "
+                    : "Please select the language you want to download the report in: "}
+                </span>
                 <FormControlLabel value={"EN"} control={<Radio />} label="EN" />
                 <FormControlLabel value={"CN"} control={<Radio />} label="CN" />
               </RadioGroup>
@@ -82,9 +98,7 @@ const ExportResult = () => {
           textChinese={"下载"}
           onClick={downloadReport}
           disabled={!(username && language)}
-        >
-
-        </BlueButton>
+        />
       </div>
     </div>
   );
