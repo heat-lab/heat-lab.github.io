693b7e3057689f8eb7f240a8213802f559d38d5e
bennettwhong <bennettwhong@gmail.com>
2026-08-03 11:15:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,135 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import Home from "./Home/Home";
-import UserValidation from "./Home/UserValidation";
-import ParentQuestions from "./Home/ParentQuestions";
-import TestSelection from "./Test Selection/TestSelection";
+import UserValidation from (
+  "./Home/UserValidation"
+);
+import ParentQuestions from (
+  "./Home/ParentQuestions"
+);
+import TestSelection from (
+  "./Test Selection/TestSelection"
+);
 import Test from "./Tests/Test";
-import StoryTest from "./StoryTest/StoryTest";
-import ExportResult from "./ExportResult";
+import StoryTest from (
+  "./StoryTest/StoryTest"
+);
+import ExportResult from (
+  "./ExportResult"
+);
+import SideCamera from (
+  "./Components/SideCamera"
+);
+
 
 function App() {
+  const cameraSession =
+    new URLSearchParams(
+      window.location.search
+    ).get("cameraSession");
+
+  if (cameraSession) {
+    return (
+      <SideCamera
+        sessionId={cameraSession}
+      />
+    );
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<UserValidation />} />
-        <Route path="/parent-questions" element={<ParentQuestions />} />
-        <Route path="/test-selection" element={<TestSelection />} />
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<UserValidation />}
+        />
+
+        <Route
+          path="/parent-questions"
+          element={<ParentQuestions />}
+        />
+
+        <Route
+          path="/test-selection"
+          element={<TestSelection />}
+        />
+
         <Route
           path="/matching-test-chinese"
-          element={<Test type="matching" language="chinese" />}
+          element={
+            <Test
+              type="matching"
+              language="chinese"
+            />
+          }
         />
+
         <Route
           path="/matching-test-english"
-          element={<Test type="matching" language="english" />}
+          element={
+            <Test
+              type="matching"
+              language="english"
+            />
+          }
         />
+
         <Route
           path="/repetition-test-chinese"
-          element={<Test type="repetition" language="chinese" />}
+          element={
+            <Test
+              type="repetition"
+              language="chinese"
+            />
+          }
         />
+
         <Route
           path="/repetition-test-english"
-          element={<Test type="repetition" language="english" />}
+          element={
+            <Test
+              type="repetition"
+              language="english"
+            />
+          }
         />
+
         <Route
           path="/story-test-chinese"
-          element={<StoryTest language="chinese" />}
+          element={
+            <StoryTest
+              language="chinese"
+            />
+          }
         />
+
         <Route
           path="/story-test-english"
-          element={<StoryTest language="english" />}
+          element={
+            <StoryTest
+              language="english"
+            />
+          }
         />
-        <Route path="/download-report" element={<ExportResult />} />
+
+        <Route
+          path="/download-report"
+          element={<ExportResult />}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
